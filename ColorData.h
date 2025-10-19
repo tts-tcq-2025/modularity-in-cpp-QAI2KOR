76c5dfb5df9e7b29d1e09c2d45219f20a917a8a3
QAI2KOR <127721803+QAI2KOR@users.noreply.github.com>
2025-10-19 18:44:00 +0530

    Create ColorData.h

---
+++ ColorData.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "ColorPair.h"
+
+namespace TelCoColorCoder
+{
+    // Function declarations for color-number conversion
+    ColorPair GetColorFromPairNumber(int pairNumber);
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor);
+}
