52153ac7c99c77cea17f638fa61b6ef16c0cc8b0
QAI2KOR <127721803+QAI2KOR@users.noreply.github.com>
2025-10-19 18:42:36 +0530

    Update ColorMappingManual.h

--- ColorMappingManual.h
+++ ColorMappingManual.h
@@ -1,6 +1,11 @@
-#pragma once
+#pragma once   
+#include <string>
 
 namespace TelCoColorCoder
-{
-    void PrintColorCodeManual();
+{   
+    // Manual formatting functions (testable components)
+    std::string FormatTableHeader();
+    std::string FormatPairEntry(int pairNumber);
+    std::string FormatTableFooter(int totalPairs);
+    std::string FormatColorCodingManual();
 }
