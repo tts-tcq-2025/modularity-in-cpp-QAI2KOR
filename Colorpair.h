fe288843d112d028c759c6e58d9ca5e491819e88
QAI2KOR <127721803+QAI2KOR@users.noreply.github.com>
2025-10-19 18:49:54 +0530

    Update Colorpair.h

--- Colorpair.h
+++ Colorpair.h
@@ -8,21 +8,21 @@ namespace TelCoColorCoder
     enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
     extern const char* const MajorColorNames[];
-    extern const int numberOfMajorColors;
     extern const char* const MinorColorNames[];
+    extern const int numberOfMajorColors;
     extern const int numberOfMinorColors;
 
     class ColorPair {
-        private:
-            MajorColor majorColor;
-            MinorColor minorColor;
-        public:
-            explicit ColorPair(MajorColor major, MinorColor minor);
-            MajorColor getMajor() const;
-            MinorColor getMinor() const;
-            std::string ToString() const;
+    private:
+        MajorColor majorColor;
+        MinorColor minorColor;
+
+    public:
+        explicit ColorPair(MajorColor major, MinorColor minor);
+        MajorColor getMajor() const;
+        MinorColor getMinor() const;
+        std::string ToString() const;
     };
 
     ColorPair GetColorFromPairNumber(int pairNumber);
-    int GetPairNumberFromColor(MajorColor major, MinorColor minor);
 }
